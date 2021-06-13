6a76414edd303ca63a614d3b901bbfdd35f53360
tommygranstrom <tommy.granstrom@hotmail.se>
2021-06-13 12:33:54 +0200

    Can write to file

--- inc/Dest.h
+++ inc/Dest.h
@@ -7,4 +7,12 @@ class Dest
         int id;
         TTime goTime;
         TTime arrTime;
+        bool operator<(const Dest& rhs) const;
+        std::string getGoStr(){return std::to_string(goTime.date)+":"+std::to_string(goTime.hour)+":"+std::to_string(goTime.minute)+":"+std::to_string(goTime.second);};
+        std::string getArrStr(){return std::to_string(arrTime.date)+":"+std::to_string(arrTime.hour)+":"+std::to_string(arrTime.minute)+":"+std::to_string(arrTime.second);};
 };
+
+inline bool Dest::operator<(const Dest& rhs) const
+{
+    return (goTime<rhs.goTime);
+}
